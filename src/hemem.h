a73c092b36c78410797b322d79f06b64f9e747f3
sach-12 <sachinshankar2010@gmail.com>
2025-03-13 12:48:30 -0700

    removed earlier approach variables from hemem.h

--- src/hemem.h
+++ src/hemem.h
@@ -185,10 +185,6 @@ struct hemem_page {
   UT_hash_handle hh;
   struct hemem_page *next, *prev;
   struct fifo_list *list;
-
-  uint64_t last_access_offset;
-  uint64_t stride;
-  int stride_count;
 };
 
 static inline uint64_t pt_to_pagesize(enum pagetypes pt)
